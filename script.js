05b7741db29091864dbd56ebde2aa0fce3c5140d
Glauber Ribeiro <glauberbarbacoviribeiro@gmail.com>
2024-11-07 10:09:05 -0300

    feat: add and organized images

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const images = ['/mybiography/images/11zon_cropped.jpeg', 
-    '/mybiography/images/IMG-20190204-WA0068.jpg']; 
+//one-part
+const imagesOne = ['/mybiography/images/glauber-ar-image.jpeg', 
+  '/mybiography/images/glauber-family-image.jpg'
+]; 
     
-const imgElement = document.getElementById('image');
+const imgElementOne = document.getElementById('image-one');
+let currentIndexOne = 0;
+
+function changeImageOne() {
+  currentIndexOne = (currentIndexOne + 1) % imagesOne.length; 
+  imgElementOne.src = imagesOne[currentIndexOne];
+}
+setInterval(changeImageOne, 10000);
+
+
+
+//two-part
+const imagesTwo = ['/mybiography/images/glauber-studio-image.jpg',
+  '/mybiography/images/class-cc-image.jpg'
+]; 
+  
+const imgElement = document.getElementById('image-two');
 let currentIndex = 0;
 
-function changeImage() {
-  currentIndex = (currentIndex + 1) % images.length; 
-  imgElement.src = images[currentIndex];
+function changeImageTwo() {
+currentIndex = (currentIndex + 1) % imagesTwo.length; 
+imgElement.src = imagesTwo[currentIndex];
 }
-setInterval(changeImage, 5000);
+setInterval(changeImageTwo, 10000);
